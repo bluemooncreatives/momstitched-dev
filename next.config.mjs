6258301789db326ce888afbc4090616b6cc7d830
Pratik Das <pratikdasmartingarrix@gmail.com>
2026-07-01 02:28:31 +0530

    feat: add 90 as a supported image quality in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -63,6 +63,7 @@ const nextConfig = {
         ]
     },
     images: {
+        qualities: [90],
         formats: ['image/avif', 'image/webp'],
         minimumCacheTTL: 60 * 60 * 24 * 7,
         deviceSizes: [320, 420, 640, 768, 1024, 1200, 1440, 1920],
